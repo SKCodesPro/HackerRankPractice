beaad16be7fc72ee4cb50f0be0eb723363ff2886
SKCodesPro <sathish.k.prakasam@gmail.com>
2024-02-01 07:01:16 +0530

    PermutinArray

--- src/main/java/org/example/lrucache/LRUCacheImpl.java
+++ src/main/java/org/example/lrucache/LRUCacheImpl.java
@@ -15,17 +15,25 @@ public class LRUCacheImpl implements LRUCache
 
     @Override
     public void put(int key, int value) {
+        // If the size of the Linked list is greater than the capacity
+        // remove the last element from the linked list which maintains the order of access of keys
+        // The frequency of access is decided in the get method
+        // Once the
         if(orderList.size() >= capacity){
             int keyRemoved = orderList.removeLast();
             cache.remove(keyRemoved);
 
         }
+        // Once the eviction is done add the key to first of linked list
         orderList.addFirst(key);
         cache.put(key, value);
     }
 
     @Override
     public int get(int key) {
+        // If the cache contains the key get denotes its recently accessed
+        // So this key has to go to the top of the recently used items
+        // Reordering of the keys happens below
         if(cache.containsKey(key)){
             orderList.remove((Object) key);
             orderList.addFirst(key);
@@ -41,6 +49,9 @@ public class LRUCacheImpl implements LRUCache
         lruCache.get(2);
         lruCache.get(1);
         lruCache.put(3, 3);
+        lruCache.get(1);
+        lruCache.put(4, 4);
+
             for(int key: lruCache.orderList){
                 System.out.println(lruCache.cache.get(key));
             }
